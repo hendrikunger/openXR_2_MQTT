f6c3472dffc68d970e83de2391bdc0e5e962fa1d
Hendrik Unger <unger.hendrik@gmail.com>
2023-01-16 16:15:19 +0100

    Initial Commit

---
+++ pyopenxr_examples-main/xr_examples/green_blue.py
@@ -0,0 +1,32 @@
+"""
+pyopenxr example program green_blue.py
+
+Different way to get similar display to the venerable gl_example.py
+"""
+
+import os
+from OpenGL import GL
+import xr
+
+
+# ContextObject is a high level pythonic class meant to keep simple cases simple.
+with xr.ContextObject(
+    instance_create_info=xr.InstanceCreateInfo(
+        enabled_extension_names=[
+            # A graphics extension is mandatory (without a headless extension)
+            xr.KHR_OPENGL_ENABLE_EXTENSION_NAME,
+        ],
+    ),
+) as context:
+    eye_colors = [
+        (0, 1, 0, 1),  # Left eye green
+        (0, 0, 1, 1),  # Right eye blue
+        (1, 0, 0, 1),  # Third eye blind
+    ]
+    for frame_index, frame_state in enumerate(context.frame_loop()):
+        for view_index, view in enumerate(context.view_loop(frame_state)):
+            # set each eye to a different color (not working yet...)
+            GL.glClearColor(*eye_colors[view_index])
+            GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+        if frame_index > 500:  # Don't run forever
+            break
---
+++ pyopenxr_examples-main/xr_examples/pink_world.py
@@ -0,0 +1,24 @@
+"""
+pyopenxr example program pink_world.py
+This example renders a solid pink field to each eye.
+"""
+
+from OpenGL import GL
+import xr
+
+
+# ContextObject is a high level pythonic class meant to keep simple cases simple.
+with xr.ContextObject(
+    instance_create_info=xr.InstanceCreateInfo(
+        enabled_extension_names=[
+            # A graphics extension is mandatory (without a headless extension)
+            xr.KHR_OPENGL_ENABLE_EXTENSION_NAME,
+        ],
+    ),
+) as context:
+    for frame_index, frame_state in enumerate(context.frame_loop()):
+        for view in context.view_loop(frame_state):
+            GL.glClearColor(1, 0.7, 0.7, 1)  # pink
+            GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+        if frame_index > 500:  # Don't run forever
+            break
